9bab627453380e343aff2cf1788e921b1f83667e
bcoachs <bjoern.stelter@gmail.com>
2026-02-01 17:53:45 +0100

    Add safe debug require for metrics

--- api/metrics.js
+++ api/metrics.js
@@ -1,12 +1,20 @@
 module.exports = (req, res) => {
   res.setHeader('Content-Type', 'application/json; charset=utf-8');
 
-  const symbolRaw = (req.query.symbol || '').toString().trim();
-  const symbol = symbolRaw.toUpperCase();
+  try {
+    // eslint-disable-next-line global-require, import/no-extraneous-dependencies
+    const yahooFinance = require('yahoo-finance2');
 
-  res.status(200).json({
-    ok: true,
-    symbol,
-    note: 'metrics route alive'
-  });
+    res.status(200).json({
+      ok: true,
+      note: 'require succeeded',
+      keys: Object.keys(yahooFinance)
+    });
+  } catch (error) {
+    res.status(200).json({
+      ok: false,
+      note: 'require failed',
+      error: error instanceof Error ? error.message : String(error)
+    });
+  }
 };
